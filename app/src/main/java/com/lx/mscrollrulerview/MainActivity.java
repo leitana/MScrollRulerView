1d5a5b2ff1931d042b8e93a1aadbdbe88fab17c9
linxiao <linx@taxsoft.cn>
2022-03-24 15:06:09 +0800

    震动

--- app/src/main/java/com/lx/mscrollrulerview/MainActivity.java
+++ app/src/main/java/com/lx/mscrollrulerview/MainActivity.java
@@ -2,16 +2,32 @@ package com.lx.mscrollrulerview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Vibrator;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
     private ScrollRulerView rulerView;
+    private Vibrator vibrator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
         rulerView = (ScrollRulerView) findViewById(R.id.rulerView);
         rulerView.setCurrentValue(5);
+        rulerView.setOnValueChangedListener(new ScrollRulerView.OnValueChangedListener() {
+            @Override
+            public void onValueChanged(float value) {
+                if (value % 10 == 0.0) {
+                    vibrator.vibrate(10);
+                } else {
+                    vibrator.vibrate(5);
+                }
+
+            }
+        });
     }
 }
